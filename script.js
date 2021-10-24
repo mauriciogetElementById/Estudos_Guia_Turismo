1dcc6947a9260659c58a9de320d19275490919a7
Mauricio Silva dos Santos <mauriciossantos2222@gmail.com>
2021-10-23 21:33:46 -0300

    Primeiro repositório conectado ao GitHub

---
+++ script.js
@@ -0,0 +1,36 @@
+var hora = 01;
+
+if (hora > 0 && hora < 6){
+    alert("Agora é de madrugada"+" "+hora);
+}
+    else if (hora < 12){
+        alert("Agora é de manhã"+" "+hora);
+    }
+        else if (hora < 18){
+            alert("Agora é tarde"+" "+hora);
+        }
+        else 
+            alert("Agora é noite"+" "+hora);
+
+
+
+document.getElementsByClassName("btn_adicionar")[0].innerHTML = "ADD";
+
+
+function verde() {
+    document.querySelector("#teste").classList.remove('vermelho');
+    document.querySelector("#teste").classList.remove('azul');
+    document.querySelector("#teste").classList.add('verde');
+}
+
+function azul() {
+    document.querySelector("#teste").classList.remove('vermelho');
+    document.querySelector("#teste").classList.remove('verde');
+    document.querySelector("#teste").classList.add('azul');
+}
+
+function vermelho() {
+    document.querySelector("#teste").classList.remove('verde');
+    document.querySelector("#teste").classList.remove('azul');
+    document.querySelector("#teste").classList.add('vermelho');
+}
